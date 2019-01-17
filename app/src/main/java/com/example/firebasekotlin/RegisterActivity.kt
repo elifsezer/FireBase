f76586946cb24d907ba00e21dc858bceb76e1ddf
elifsezer <ellfsezer60@gmail.com>
2019-01-17 15:41:32 +0300

    Firebase işlemleri

---
+++ app/src/main/java/com/example/firebasekotlin/RegisterActivity.kt
@@ -0,0 +1,68 @@
+package com.example.firebasekotlin
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.shashank.sony.fancytoastlib.FancyToast
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        btnKayit.setOnClickListener {
+            if (etMail.text.isNotEmpty() && etSifre.text.isNotEmpty() && etSifreTekrar.text.isNotEmpty()) {
+                if (etSifre.text.toString().equals(etSifreTekrar.text.toString())) {
+                    yeniUyeKayit(etMail.text.toString(), etSifre.text.toString())
+                } else {
+                    FancyToast.makeText(this, "Şifreler Eşleşmiyor", FancyToast.LENGTH_SHORT, FancyToast.ERROR, true)
+                        .show()
+
+                }
+            } else {
+                FancyToast.makeText(
+                    this,
+                    "Lütfen Boş Alanları Doldurunuz",
+                    FancyToast.LENGTH_SHORT,
+                    FancyToast.WARNING,
+                    true
+                ).show()
+            }
+
+        }
+    }
+
+    private fun yeniUyeKayit(mail: String, sifre: String) {
+        progressBarGoster()
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(mail, sifre)
+            .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+                //yukarıdaki işlem başarılı bir şekilde yapıldıysa aşağısı tetiklencek.
+                override fun onComplete(p0: Task<AuthResult>) {
+                    if (p0.isSuccessful) {
+                        Toast.makeText(this@RegisterActivity, "Üye Kayıt Edildi."+" "+FirebaseAuth.getInstance().currentUser?.uid, Toast.LENGTH_SHORT).show()
+                        FirebaseAuth.getInstance().signOut()
+                    } else {
+                        Toast.makeText(this@RegisterActivity, "Üye Kayıt Edilirken Sorun Oluştu."+" "+p0.exception?.message, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                }
+            })
+        progressBarGizle()
+
+    }
+
+    private fun progressBarGoster() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun progressBarGizle() {
+        progressBar.visibility = View.INVISIBLE
+    }
+}
