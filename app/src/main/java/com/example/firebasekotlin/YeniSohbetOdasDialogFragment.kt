9696671745eb7a9006de722ae0b9a45c70f813d2
elifsezer <ellfsezer60@gmail.com>
2019-01-31 22:27:33 +0300

    "Sohbet Ayarlari 2"

--- app/src/main/java/com/example/firebasekotlin/YeniSohbetOdasDialogFragment.kt
+++ app/src/main/java/com/example/firebasekotlin/YeniSohbetOdasDialogFragment.kt
@@ -40,11 +40,9 @@ class YeniSohbetOdasDialogFragment : DialogFragment() {
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onStopTrackingTouch(p0: SeekBar?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
         })
